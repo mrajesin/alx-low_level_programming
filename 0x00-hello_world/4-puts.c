51833fe738ff8bbef43a4c9e3209a0de4ccd4d08
mrajesin <ajesin.com@gmail.com>
2022-09-08 13:40:38 +0000

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * A program to display
- * an output on a new line
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
